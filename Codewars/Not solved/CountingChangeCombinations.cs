14f592baab6a8282671b2a6901da40dfbfc6431e
daure <daure@LAPTOP-CS0Q4Q4B>
2021-09-17 15:26:06 +0600

    Revert "Commented"
    This reverts commit a8d155c15497392025629a13fba59e124865b800.

--- Codewars/Not solved/CountingChangeCombinations.cs
+++ Codewars/Not solved/CountingChangeCombinations.cs
@@ -1,20 +1,29 @@
-﻿namespace Codewars.Not_solved
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.CSharp;
+using System.Numerics;
+
+namespace Codewars.Not_solved
 {
     class CountingChangeCombinations
     {
-        //public static int CountCombinations(int money, int[] coins)
-        //{
-        //    int[] ways = new int[money + 1];
-        //    ways[0] = 1;
+        public static int CountCombinations(int money, int[] coins)
+        {
+            int[] ways = new int[money + 1];
+            ways[0] = 1;
 
-        //    for (int i = 0; i < coins.Length; i++)
-        //    {
-        //        for (int j = coins[i]; j <= money; j++)
-        //        {
-        //            ways[j] += ways[j - coins[i]];
-        //        }
-        //    }
-        //    return ways[money];
-        //}
+            for (int i = 0; i < coins.Length; i++)
+            {
+                for (int j = coins[i]; j <= money; j++)
+                {
+                    ways[j] += ways[j - coins[i]];
+                }
+            }
+            return ways[money];
+        }
     }
 }
